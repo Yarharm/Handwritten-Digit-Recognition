baaed0a6ef8a4b9641e001c776008b653d794ff6
Yarharm <yarharm@gmail.com>
2019-01-03 00:18:17 -0500

    PeP8 code

--- Neural Network.py
+++ Neural Network.py
@@ -2,120 +2,155 @@ import numpy as np
 import scipy.special as spec
 import matplotlib.pyplot as plt
 
-# 3 layer neura network with stochastic gradient descend
+
 class NeuralNetwork:
-    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
-        # number of nodes for the input, hidden and output layers
-        self.inodes = input_nodes
-        self.hnodes = hidden_nodes
-        self.onodes = output_nodes
+    """ Three layers Neural Network with the Stochastic Gradient Descent
 
-        # learning rate
-        self.lr = learning_rate
+        Parameters
+        ------------
+        inodes : int
+            Number of nodes in the first layer of the network.
+        hnodes : int
+            Number of nodes in the second layer of the network.
+        onodes : int
+            Number of nodes in the third layer of the network.
+        eta : float
+            Learning rate (between 0.0 and 1.0).
 
-        # matrices wih(input => hidden) and who(hidden => output)
-        # weights in range of normal distribution with standard div 1/sqrt(n)
+        Attributes
+        ------------
+        wih : {array-like}, shape = [n_hnodes, n_inodes]
+            Weights between 1 and 2 layers of the network, where
+            n_hnodes is a number of nodes in the 2nd layer and
+            n_inodes is a number of nodes in the 1st layer
+
+        who : {array-like}, shape = [n_onodes, n_hnodes]
+            Weights between 2 and 3 layers of the network, where
+            n_onodes is a number of nodes in 3rd layer and
+            n_hnodes is a number of nodes in the 2nd layer
+
+        activation_func : float
+            Sigmoid function (between 0.0 and 1.0)
+    """
+    def __init__(self, inodes, hnodes, onodes, eta=0.1):
+        self.inodes = inodes
+        self.hnodes = hnodes
+        self.onodes = onodes
+        self.eta = eta
+
+        # Weights in range of normal distribution with standard div 1/sqrt(n)
         self.wih = np.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
         self.who = np.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
 
-        # activation function (Sigmoid = expit)
         self.activation_func = lambda x: spec.expit(x)
 
-
-    # adjust the weights
     def train(self, input_list, target_list):
-        # targets => training data
+        """ Train the network.
+
+        :param input_list : {array-like}, shape = [n_records, n_features]
+            Data for the training features, where n_records is the number of samples
+            and n_features is the number of features.
+        :param target_list : array-like, shape = [n_records]
+            Data for the target feature.
+
+        :return : None
+        """
+        # Targets => training data
         targets = np.array(target_list, ndmin=2).T
         inputs = np.array(input_list, ndmin=2).T
 
-        # propagate through network
+        # Propagate through network
         hidden_in = np.dot(self.wih, inputs)
         hidden_out = self.activation_func(hidden_in)
         final_in = np.dot(self.who, hidden_out)
         final_out = self.activation_func(final_in)
 
-        # error (target - actual)
         # output_errors => Refine weights between hidden and final layers
         # hidden_errors => Refine weight between input and hidden layers
         output_errors = targets - final_out
         hidden_errors = np.dot(self.who.T, output_errors)
 
-        # update weights between hidden and output layers
+        # Update weights between hidden and output layers
         self.who += self.lr * np.dot((output_errors * final_out * (1.0 - final_out)), np.transpose(hidden_out))
 
-        # uodate weights between input and hidden layers
+        # Uodate weights between input and hidden layers
         self.wih += self.lr * np.dot((hidden_errors * hidden_out * (1.0 - hidden_out)), np.transpose(inputs))
 
-
-    # get output from the neural network
     def query(self, input_list):
-        # convert inputs into 2-d array for the dot product
-        # |.T| => transpose
+        """ Return predicted number (between 0 and 9)
+
+        :param input_list : {array-like}, shape = [n_records, n_features]
+            Data for the training features, where n_records is the number of samples
+            and n_features is the number of features.
+
+        :return : array of n_onodes floats
+            highest float in the array corresponds to the predicted index,
+            where n_onodes is a number of nodes in the 3rd layer.
+        """
+        # Convert inputs into 2-d array for the dot product
         inputs = np.array(input_list, ndmin=2).T
 
-        # calculate hidden layer (Applying Sigmoid on the dot product)
+        # Calculate hidden layer (Applying Sigmoid on the dot product)
         hidden_in = np.dot(self.wih, inputs)
         hidden_out = self.activation_func(hidden_in)
 
-        # calculate final layer
+        # Calculate final layer
         final_in = np.dot(self.who, hidden_out)
         return self.activation_func(final_in)
 
-# Main
+# Input, hidden and output nodes
+input_nodes = 784  # 28x28 image
+hidden_nodes = 100  # any reasonable number between 10 and 784, to avoid overfitting and underfitting
+output_nodes = 10  # number of possible numbers between [0..9]
 
-# input, hidden and output nodes
-input_nodes = 784 # 28x28 image
-hidden_nodes = 100 # any reasonable number between 10 and 784, to avoid overfitting and underfitting
-output_nodes = 10 # number of possible numbers between [0..9]
+# Experimentally derived, through brute force( GridSearch for the hyperparameter tuning )
+eta = 0.2
 
-# Experimentally derived, through brute force
-learning_rate = 0.2
-
-# passes over the training dataset
+# Passes over the training dataset
 epochs = 2
 
-# create NN
-nn = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+# Create NN
+nn = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, eta)
 
 # TRAINING PHASE (For 2 epochs
-# load training data from the subset of MNIST.csv of 100 elements
+# Load training data from the subset of MNIST.csv of 100 elements
 training_data = open("MNIST/mnist_train_10k.csv", "r")
 training_list = training_data.readlines()
 training_data.close()
 
-# train NN
+# Train NN
 for e in range(epochs):
     for label in training_list:
-        # rescale inputs from the training_list in range (0.00, 1.00]
+        # Rescale inputs from the training_list in range (0.00, 1.00]
         inputs = np.asfarray(label.split(',')[1:]) / 255 * 0.99 + 0.01
-        # create target array full of 0.01 and label of 0.99
+        # Create target array full of 0.01 and label of 0.99
         targets = np.zeros(output_nodes) + 0.01
         targets[int(label.split(',')[0])] = 0.99
         nn.train(inputs, targets)
 
 # TESTING PHASE
-# load testing data from the subset of MNIST.csv of 10 elements
+# Load testing data from the subset of MNIST.csv of 10 elements
 testing_data = open("MNIST/mnist_test_1k.csv", "r")
 testing_list = testing_data.readlines()
 testing_data.close()
 
-# score for each record in the test set
+# Score for each record in the test set
 score = []
 
 for record in testing_list:
     correct_answer = int(record.split(',')[0])
-#    print(correct_answer, "correct answer")
-    #get inputs
+    # Print(correct_answer, "correct answer")
+    # Get inputs
     inputs = np.asfarray(record.split(',')[1:]) / 255.0 * 0.99 + 0.01
     outputs = nn.query(inputs)
-    # get label spitted by the NN
+    # Get label spitted by the NN
     answer = np.argmax(outputs)
-#    print(answer, "network output")
-    # add 1 => correct answer/ 0 => incorrect
+    # Print(answer, "network output")
+    # Add 1 => correct answer/ 0 => incorrect
     if(answer == correct_answer):
         score.append(1)
     else:
         score.append(0)
-
+# Evaluate performance
 score_array = np.asarray(score)
 print("Performance in %: ", score_array.sum() / score_array.size)
